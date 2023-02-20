2d4d3938a34b13db0f6d8fcfa27da22ff38c7952
Myhailo Nyskohuz <61565249+65d@users.noreply.github.com>
2023-02-20 11:16:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const port = 3000
 
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "1111"
-});
+// var con = mysql.createConnection({
+//     host: "127.0.0.1",
+//     port: 3306,
+//     user: "root",
+//     password: "1111"
+// });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
