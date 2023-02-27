37e5965ab06f0fbd9624b4ca0f50fe55cea3ca8f
Myhailo Nyskohuz <61565249+65d@users.noreply.github.com>
2023-02-27 14:22:49 +0100

    test

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.get('/', (req, res) => {
 });
 
 // Запуск сервера
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+    console.log('Server started on port 3000');app.listen(3000, () => {
+
+    });
